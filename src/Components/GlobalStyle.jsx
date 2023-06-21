7f4132aac0e1d0ad6c5ad96ba22f479620b138bd
Evandrosc <evandromdsc@gmail.com>
2023-06-21 19:51:27 -0300

    Revert "Merge branch 'main' of https://github.com/Evandrosc/provedorInternet"
    This reverts commit 6477682ee1b276e0c986912fe7a0bffded75b089, reversing
    changes made to 6c6438779d33675b3b08164ed9298c100d3bcdd0.

--- src/Components/GlobalStyle.jsx
+++ src/Components/GlobalStyle.jsx
@@ -4,7 +4,6 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
-    font-display: swap;
     margin: 0;
     padding: 0;
     text-decoration: none;
