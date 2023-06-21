27225c3c22f9155a34e4048c6d10fb9640e487a7
Evandrosc <evandromdsc@gmail.com>
2023-06-21 19:35:32 -0300

    seo font-fammily

--- src/Components/GlobalStyle.jsx
+++ src/Components/GlobalStyle.jsx
@@ -4,6 +4,7 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+    font-display: swap;
     margin: 0;
     padding: 0;
     text-decoration: none;
